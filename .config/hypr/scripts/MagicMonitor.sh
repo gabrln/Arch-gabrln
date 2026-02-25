c75a8281dca2738f5f845e4357783fee13b3779e
Gabriel de Souza <167147017+gsouza38@users.noreply.github.com>
2026-02-25 19:07:40 -0300

    Update MagicMonitor.sh

--- .config/hypr/scripts/MagicMonitor.sh
+++ .config/hypr/scripts/MagicMonitor.sh
@@ -4,25 +4,35 @@
 # OBJETIVO: Ouvir o Hyprland e enviar notificações Toast para o Noctalia
 # ═══════════════════════════════════════════════════════════════════════════
 
-# Proteção: Verifica se o socat está instalado
 if ! command -v socat &>/dev/null; then
   echo "ERRO: O socat não está instalado."
   exit 1
 fi
 
+# Variável de estado (Memória do script)
+MAGIC_ABERTO=false
+
 # Ouve o canal de eventos do Hyprland continuamente
 socat -U - UNIX-CONNECT:$XDG_RUNTIME_DIR/hypr/$HYPRLAND_INSTANCE_SIGNATURE/.socket2.sock | while read -r line; do
 
   # ─── O MAGIC FOI ABERTO ───
-  # Aspas no texto literal para proteger os '>>', e o asterisco fora para funcionar
   if [[ "$line" == "activespecial>>special:magic,"* ]]; then
-
+    MAGIC_ABERTO=true
     qs -c noctalia-shell ipc call toast send '{"title":"🪄 MODO MAGIC","body":"Workspace Especial Ativado","type":"notice","duration":3000}'
 
-  # ─── O MAGIC FOI FECHADO ───
+  # ─── OUTRO SPECIAL FOI ABERTO (Ex: Terminal) ───
+  # Se abrir o terminal, garantimos que a memória sabe que o magic não está a uso
+  elif [[ "$line" == "activespecial>>special:"* ]]; then
+    MAGIC_ABERTO=false
+
+  # ─── UM SPECIAL FOI FECHADO ───
   elif [[ "$line" == "activespecial>>,"* ]]; then
 
-    qs -c noctalia-shell ipc call toast send '{"title":"🪄 MODO MAGIC","body":"Workspace Especial Desativado","type":"notice","duration":2000}'
+    # Só envia a notificação de saída SE o magic era o que estava aberto
+    if [ "$MAGIC_ABERTO" = true ]; then
+      qs -c noctalia-shell ipc call toast send '{"title":"🪄 MODO MAGIC","body":"Workspace Especial Desativado","type":"notice","duration":2000}'
+      MAGIC_ABERTO=false # Reseta a memória
+    fi
 
   fi
 
